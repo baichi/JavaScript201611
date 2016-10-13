3285366b2817c339ef1be616022afb0bd18dd2b7
Tian Xi <tianxiallen@163.com>
2016-10-13 16:35:44 +0800

    表格排序

--- 正式课/第二周（call&表格排序和正则）/第三天( 表格排序 )/2 表格排序/js/table.js
+++ 正式课/第二周（call&表格排序和正则）/第三天( 表格排序 )/2 表格排序/js/table.js
@@ -7,7 +7,8 @@ var tHeadRow = tHead.rows[0]; //tHead下所有行中的第一行
 var ths = tHeadRow.cells; //表头第一行下所有的单元格(列)
 //console.log(ths);
 var tBody = table.tBodies[0]; //获取表格所有tBody中的第一个
-
+var tBodyTrs = tBody.rows; //获取tBody下所有的行
+console.log(tBodyTrs); //这里并不存在任何一行，但是在绑定数据之后并不用重新获取，这是由于dom的映射 => 获取的元素集合会随着页面内元素的增加或者删除，集合的长度也会随之增加或者删除
 // 在data.txt这个文件中伪造了一些数据，由于这个数据都是在txt文本文件中，所以这些数据暂时就是一个字符串，如果想换成JSON格式还需要处理
 // 通过Ajax来获取这些数据
 /*
@@ -49,7 +50,78 @@ var tBody = table.tBodies[0]; //获取表格所有tBody中的第一个
 console.log(data);
 // 数据已经成功获取到，现在需要把获取到的数据，添加到页面中 => 数据绑定
 
+;(function bindData(){
+    if(window.data){ // 要么是数组或者undefined
+        //只要是数组我就可以循环，数组里的每一项在表格中就是一行 => tbody的行数和length是相同的
+        var frg = document.createDocumentFragment(); //创建文档碎片
+        for(var i=0; i<data.length; i++){ //循环执行的次数取决于data里有多少个对象。每一个对象都是一个tr
+            var curData = data[i]; //curData就是每次循环时候数组中每一个对象，其实也就是每一条数据,curData都包含5个属性。name,sex,age,height,weight
+            var tr = document.createElement('tr'); //创建一个行
+            // 向tr这个行中添加列,每个列的标签都是td
+            for(var key in curData){ //只要能循环一次，就需要创建一个td。td跟有多少组属性是相同的
+                var td = document.createElement('td');
+                // td里的内容就是curDate的属性值了
+                if(key === 'sex'){ //说明才是循环到性别
+                    //if(curData[key] == 1){
+                    //    td.innerHTML = '男';
+                    //}else{
+                    //    td.innerHTML = '女';
+                    //}
+                    td.innerHTML = curData[key] == 1 ? "男" : "女";
+                }else{
+                    td.innerHTML = curData[key];
+                }
 
+                tr.appendChild(td);
+            }
+
+            frg.appendChild(tr); // 把行添加到文档碎片
+        }
+        tBody.appendChild(frg); // 把文档碎片添加到tbody
+        frg = null;
+    }
+})();
+
+//console.log(tBodyTrs); //不用重新获取
+// 隔行变色
+function changeColor(){
+    for(var i=0; i<tBodyTrs.length; i++){
+        tBodyTrs[i].className = 'c' + i%2;
+    }
+}
+changeColor();
+
+// 排序: 就是tbody里的行(tr) 按照 年龄/ 身高/ 体重 .. 上下移动
+
+function sort(){
+    var tBodyTrsAry = utils.listToArray(tBodyTrs); //把类数组转化成数组
+    tBodyTrsAry.sort(function (a,b){
+        // a,b分别都是代表tbody下面的行。是按照行下面列的innerHTML排序
+        return a.cells[2].innerHTML - b.cells[2].innerHTML;
+    });
+    //把排好序的数组(表格下的所有行)还需要回填到表格中，这样才是排序
+    var frg = document.createDocumentFragment();
+    for(var i=0; i<tBodyTrsAry.length; i++){
+        frg.appendChild(tBodyTrsAry[i]);
+    }
+    tBody.appendChild(frg);
+    frg = null;
+}
+
+// 给每一个表头th绑定事件
+;(function bindEvent(){
+    //循环绑定事件给表头
+    for(var i=0; i<ths.length; i++){
+        // 不是给所有的表头列都绑定事件，只给带cursor这个类的绑定。在绑定之前要做一个判断，判断是否存在cursor这个类
+        if(ths[i].className == 'cursor'){
+            ths[i].onclick = function (){
+                //点击的表头的时候,就应该排序了
+                sort();
+                changeColor(); // 每次排序之后隔行变色就乱了，所以需要重新执行一次
+            }
+        }
+    }
+})();
 
 
 
