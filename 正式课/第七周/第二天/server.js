090a56d700c4e3248a7006be2449175b0363ea18
zhouxiaotian <1282347298@qq.com>
2016-11-16 19:02:04 +0800

    AJAX&NODE&HTTP

--- 正式课/第七周/第二天/server.js
+++ 正式课/第七周/第二天/server.js
@@ -5,14 +5,36 @@ var server1 = http.createServer(function (request, response) {
     var urlObj = url.parse(request.url, true),
         pathname = urlObj.pathname,
         query = urlObj.query;
-    if (pathname === '/index.html') {
-        var con = fs.readFileSync('./index.html', 'utf-8');
-        response.end(con);
-    }
 
-    if (pathname === '/css/index.css') {
-        con = fs.readFileSync('./css/index.css', 'utf-8');
-        response.end(con);
+    //->项目资源文件请求处理
+    var reg = /\.([0-9a-zA-Z]+)/i;
+    if (reg.test(pathname)) {
+        //->获取当前请求资源文件的后缀名,根据文件的后缀名计算出它对应的MIME类型
+        var suffix = reg.exec(pathname)[1].toUpperCase(),
+            suffixMIME = 'text/plain';
+        switch (suffix) {
+            case 'HTML':
+                suffixMIME = 'text/html';
+                break;
+            case 'CSS':
+                suffixMIME = 'text/css';
+                break;
+            case 'JS':
+                suffixMIME = 'text/javascript';
+                break;
+        }
+
+        try {
+            var conFile = fs.readFileSync('.' + pathname, 'utf-8');
+            var status = 200;
+        } catch (e) {
+            conFile = 'NOT FOUND!';
+            suffixMIME = 'text/plain';
+            status = 404;
+        }
+        //->重写响应头信息:在响应头中告诉浏览器当前资源文件的MIME类型
+        response.writeHead(status, {'content-type': suffixMIME + ';charset=utf-8;'});
+        response.end(conFile);
     }
 });
 server1.listen(80, function () {
