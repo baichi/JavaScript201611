51dd744820d458f08dafb9f232a254c01c30abd0
zhouxiaotian <1282347298@qq.com>
2016-11-17 22:03:35 +0800

    crm

--- 正式课/第七周/crm/js/index.js
+++ 正式课/第七周/crm/js/index.js
@@ -1,30 +1,68 @@
-//->开始加载页面的时候,我们通过API文档中的接口地址获取到所有的客户信息,然后把信息展示在页面中(字符串拼接)
-ajax({
-    url: '/getAllList',
-    type: 'get',
-    dataType: 'json',
-    cache: false,
-    success: function (result) {
-        if (result && result.code == 0) {
-            var data = result.data;
-            bindHTML(data);
+var customerRender = (function () {
+    var content = document.getElementById('content');
+
+    //->实现删除
+    function bindEvent() {
+        content.onclick = function (e) {
+            e = e || window.event;
+            var tar = e.target || e.srcElement,
+                tarTag = tar.tagName.toUpperCase();
+            if (tarTag === 'A' && tar.innerHTML === '删除') {
+                var customId = tar.getAttribute('data-id');
+                var flag = confirm('确定要删除编号为 [ ' + customId + ' ] 的客户吗?');
+                if (flag) {
+                    //->点击的是确定
+                    ajax({
+                        url: '/removeInfo?id=' + customId,
+                        type: 'get',
+                        dataType: 'json',
+                        cache: false,
+                        success: function (result) {
+                            if (result && result.code == 0) {
+                                content.removeChild(tar.parentNode.parentNode);
+                            }
+                        }
+                    });
+                }
+            }
+        };
+    }
+
+
+    //->实现页面数据的绑定
+    function bindHTML(data) {
+        var str = '';
+        for (var i = 0; i < data.length; i++) {
+            var cur = data[i];
+            str += '<li>';
+            str += '<span>' + cur.id + '</span>';
+            str += '<span>' + cur.name + '</span>';
+            str += '<span>';
+            str += '<a href="detail.html?id=' + cur.id + '">修改</a>';
+            str += '<a href="javascript:;" data-id="' + cur.id + '">删除</a>';
+            str += '</span>';
+            str += '</li>';
+        }
+        content.innerHTML = str;
+    }
+
+    return {
+        init: function () {
+            ajax({
+                url: '/getAllList',
+                type: 'get',
+                dataType: 'json',
+                cache: false,
+                success: function (result) {
+                    if (result && result.code == 0) {
+                        var data = result.data;
+                        bindHTML(data);
+                        bindEvent();
+                    }
+                }
+            });
         }
     }
-});
-var content = document.getElementById('content');
-//->实现页面数据的绑定
-function bindHTML(data) {
-    var str = '';
-    for (var i = 0; i < data.length; i++) {
-        var cur = data[i];
-        str += '<li>';
-        str += '<span>' + cur.id + '</span>';
-        str += '<span>' + cur.name + '</span>';
-        str += '<span>';
-        str += '<a href="">修改</a>';
-        str += '<a href="">删除</a>';
-        str += '</span>';
-        str += '</li>';
-    }
-    content.innerHTML = str;
-}
+})();
+customerRender.init();
+
